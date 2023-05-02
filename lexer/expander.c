51496a080453d4e67e614b6617e34e6e55d6a423
Felix Georgeault <fgeorgea@student.s19.be>
2023-05-02 16:31:52 +0200

    changed expander

--- lexer/expander.c
+++ lexer/expander.c
@@ -6,7 +6,7 @@
 /*   By: fgeorgea <fgeorgea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 14:23:31 by dopeyrat          #+#    #+#             */
-/*   Updated: 2023/05/02 16:08:51 by fgeorgea         ###   ########.fr       */
+/*   Updated: 2023/05/02 16:28:46 by fgeorgea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,22 +33,27 @@ void	expand(t_token *t, t_list *head)
 	char	*value;
 	char	*tmp;
 	int		i;
-	int		j[2];
+	int		j[3];
 	int		len;
 
 	i = 0;
 	len = 0;
+	j[2] = 0;
 	while (t->word[i])
 	{
-		if (t->word[i] == '\'')
+		if (t->word[i] == '"')
+			j[2] = !j[2];
+		if (t->word[i] == '\'' && !j[2])
 			i = skip_quotes(t->word, i);
 		if (t->word[i] == '$' && t->word[i + 1] && t->word[i + 1] != ' ')
 		{
 			j[0] = i;
 			while (t->word[i] && t->word[i] != ' ')
 			{
-				if (t->word[i] == '\'' || t->word[i] == '"')
+				if ((t->word[i] == '\'' || t->word[i] == '"') && !j[2])
 					i = skip_quotes(t->word, i);
+				else if (t->word[i] == '"')
+					j[2] = !j[2];
 				i++;
 			}
 			j[1] = i;
@@ -57,13 +62,12 @@ void	expand(t_token *t, t_list *head)
 		}
 		i++;
 	}
-	tmp = ft_strndup(&t->word[i + 1], ft_strlen(t->word) - j[0] - len - 1);
+	tmp = ft_strndup(&t->word[j[0] + 1], j[1] - j[0]);
 	if (!tmp)
 	{
 		ft_lstclear(&head, &free);
 		ft_exit(EXIT_MALLOC_FAILURE);
 	}
-	printf("%s\n", tmp);
 	value = get_env_value(tmp, ft_strlen(tmp));
 	free(tmp);
 	tmp = malloc(sizeof(char) * (ft_strlen(value) + i + len + 1));
--- lexer/lexer.c
+++ lexer/lexer.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   lexer.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dopeyrat <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: fgeorgea <fgeorgea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/31 15:16:09 by dopeyrat          #+#    #+#             */
-/*   Updated: 2023/03/31 15:16:11 by dopeyrat         ###   ########.fr       */
+/*   Updated: 2023/05/02 16:30:57 by fgeorgea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,12 +79,21 @@ void	lexer(char *str)
 {
 	t_list	*words;
 	t_token	*tmp;
+	t_list	*temp;
 
 	ft_free_cmd();
 	words = pre_token(str);
 	if (!words)
 		return ;
 	tokenize(words);
+	temp = words;
+	while (temp)
+	{
+		tmp = temp->content;
+		printf("%s\n", tmp->word);
+		temp = temp->next;
+	}
+	printf("EXPANDER\n");
 	expander(words);
 	while (words)
 	{
