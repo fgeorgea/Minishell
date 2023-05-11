4900ef9999695c99b388e86f7b8d1754340427e4
Felix Georgeault <fgeorgea@student.s19.be>
2023-05-11 21:26:54 +0200

    unset undifined identifier done

--- builtins/unset.c
+++ builtins/unset.c
@@ -3,14 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   unset.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fgeorgea <fgeorgea@student.s19.be>         +#+  +:+       +#+        */
+/*   By: fgeorgea <fgeorgea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/01 18:29:11 by fgeorgea          #+#    #+#             */
-/*   Updated: 2023/05/11 01:47:32 by fgeorgea         ###   ########.fr       */
+/*   Updated: 2023/05/11 21:25:29 by fgeorgea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
+static int	is_valid_key(char *key)
+{
+	int	i;
+
+	i = 0;
+	if (!ft_isalpha(key[0]))
+		return (0);
+	while (key[i])
+	{
+		if (!ft_isalnum(key[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
 
 static void	delete_var_env(char *var)
 {
@@ -43,7 +59,14 @@ void	ft_unset(char **vars)
 		return ;
 	while (vars[i])
 	{
-		delete_var_env(vars[i]);
+		if (is_valid_key(vars[i]))
+			delete_var_env(vars[i]);
+		else
+		{
+			ft_putstr_fd("Minishell: unset: '", 2);
+			ft_putstr_fd(vars[i], 2);
+			ft_putstr_fd("': not a valid identifier\n", 2);
+		}
 		i++;
 	}
 	lst_to_array(&g_sh->env);
