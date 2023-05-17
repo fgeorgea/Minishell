b296cb04590a7463c65b9ce75c3000732d06f5b9
Felix Georgeault <fgeorgea@student.s19.be>
2023-05-17 17:55:23 +0200

    fixed redir with single builtin

--- builtins/redirections_builtins.c
+++ builtins/redirections_builtins.c
@@ -6,21 +6,47 @@
 /*   By: fgeorgea <fgeorgea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/14 16:06:31 by fgeorgea          #+#    #+#             */
-/*   Updated: 2023/05/15 15:20:52 by fgeorgea         ###   ########.fr       */
+/*   Updated: 2023/05/17 17:54:12 by fgeorgea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-static int	get_in_redir_builtin(t_cmd *cmd)
+static t_redir	*get_in_redir_builtin(t_redir **redirection)
+{
+	t_redir	*redir;
+	t_redir	*last;
+
+	if (!redirection || !*redirection)
+		return (NULL);
+	redir = *redirection;
+	last = NULL;
+	while (redir)
+	{
+		if (redir->mode == HEREDOC)
+			here_doc(redir->key);
+		if (redir->mode == HEREDOC_EXP)
+			ft_here_doc_exp(redir->key);
+		if (redir->mode == IN && !file_exist(redir->key))
+			return (redir);
+		if (redir->mode == HEREDOC || redir->mode == IN || redir->mode == HEREDOC_EXP)
+			last = redir;
+		redir = redir->next;
+		if (g_sh->here_doc_status)
+			return (0);
+	}
+	return (last);
+}
+
+static int	get_infile_builtin(t_cmd *cmd)
 {
 	int		fd;
 	t_redir	*redir;
 
-	redir = get_in_redir(&cmd->redir);
+	redir = get_in_redir_builtin(&cmd->redir);
 	if (!redir || !redir->key)
 		return (0);
-	if (!file_exist(redir->key) && redir->mode != HEREDOC)
+	if (!file_exist(redir->key) && redir->mode == IN)
 	{
 		print_err(NULL, redir->key, ": file not found\n");
 		g_sh->pipe_exit = 1;
@@ -42,7 +68,7 @@ int	builtin_redirection(void)
 	if (p->nbr_cmds > 1)
 		return (1);
 	cmd = g_sh->cmd;
-	p->infile = get_in_redir_builtin(cmd);
+	p->infile = get_infile_builtin(cmd);
 	if (p->infile == -1)
 		return (0);
 	if (p->infile > 0)
