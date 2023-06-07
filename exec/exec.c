49322f5ab69f6ee85c48621635eb0985d3e87178
Felix georgeault <gorgo.felix@gmail.com>
2023-06-07 03:04:37 +0200

    refactor child function

--- exec/exec.c
+++ exec/exec.c
@@ -3,33 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   exec.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fgeorgea <fgeorgea@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fgeorgea <fgeorgea@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/07 14:44:26 by fgeorgea          #+#    #+#             */
-/*   Updated: 2023/06/06 11:22:16 by fgeorgea         ###   ########.fr       */
+/*   Updated: 2023/06/07 03:02:11 by fgeorgea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-// All the child execution goes in here.
-static void	children(size_t pos, t_cmd *cmd, t_pipex *p)
+// Sets up the correct signals and exits if there are no cmd or error...
+static void	prepare_child(size_t pos, t_cmd *cmd, t_pipex *p)
 {
 	set_signals(CHILD);
 	g_sh->is_child = CHILD;
-	if (p->infile < 0 || p->outfile < 0)
-		exit_only_child(EXIT_FAILURE);
-	if (g_sh->here_doc_status)
-		ft_exit(EXIT_SUCCESS);
+	if (p->infile < 0 || p->outfile < 0 || g_sh->here_doc_status || !cmd->cmd)
+		exit_only_child(g_sh->pipe_exit);
 	close_pipes_children(pos, p);
+}
+
+// All the child execution goes in here.
+static void	children(size_t pos, t_cmd *cmd, t_pipex *p)
+{
+	prepare_child(pos, cmd, p);
 	if (pos == 0)
 		first_child(p);
 	else if (pos == p->nbr_pipe)
 		last_child(pos, p);
 	else
 		middle_child(pos, p);
-	if (!cmd->cmd)
-		exit_only_child(EXIT_SUCCESS);
 	check_cmd(cmd->cmd);
 	if (p->hd_tmp)
 		unlink(p->hd_tmp);
