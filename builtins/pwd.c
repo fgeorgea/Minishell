170505b79566da1d4fa387529e17d59c1f98d7d6
Felix georgeault <gorgo.felix@gmail.com>
2023-06-08 12:01:53 +0200

    got rid of silent in LIBFT Makefile

--- builtins/pwd.c
+++ builtins/pwd.c
@@ -3,19 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   pwd.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fgeorgea <fgeorgea@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fgeorgea <fgeorgea@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/28 17:17:30 by fgeorgea          #+#    #+#             */
-/*   Updated: 2023/05/28 18:58:37 by fgeorgea         ###   ########.fr       */
+/*   Updated: 2023/06/08 01:14:56 by fgeorgea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-/*
-	Will print the current directory.
-	In case getcwd crashes, it will try to print the PWD env var.
-*/
+// Prints the current working directory
 void	pwd_builtin(void)
 {
 	char	*pwd;
